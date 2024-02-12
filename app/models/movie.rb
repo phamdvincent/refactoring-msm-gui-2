aa83aa67b75e379b0cdfc6f99d8e6e391a021160
Vincent Pham <40014512+phamdvincent@users.noreply.github.com>
2024-02-12 21:30:46 +0000

    Completed refactoring

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -16,6 +16,16 @@ class Movie < ApplicationRecord
   validates(:director_id, presence: true)
   validates(:title, uniqueness: true)
 
+  has_many(:characters, class_name: "Character", foreign_key: "movie_id")
+  
+  belongs_to(:director)
+
+  # def characters
+  #   key = self.id
+  #   the_many = Character.where({ :movie_id => key })
+  #   return the_many
+  # end
+
   def director
     key = self.director_id
 
